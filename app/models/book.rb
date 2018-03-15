1a7ab2e9fb8abfd32bfb98c91f0e549bcb29289c
kenishihara <stonevillagehill@gmail.com>
2018-03-15 16:06:31 +0000

    バリデーション追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :author, presence: true
 end
