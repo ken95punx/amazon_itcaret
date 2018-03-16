22e18c7f15e33ed411b8cdc27c891873088386f9
kenishihara <stonevillagehill@gmail.com>
2018-03-16 08:15:47 +0000

    一覧ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,43 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all.order(release_date: "desc")
   end
 
-  def new
-  end
+  # def show
+  #   @book = Book.find(params[:id])
+  # end
 
-  def create
-  end
+  # def new
+  #   @book = Book.new
+  # end
+
+  # def create
+  #   # render plain: params[:post].inspect
+  #   @book = Book.new(
+  #     title: params[:title],
+  #     price: params[:price],
+  #     release_date: params[:release_date],
+  #     image: "default_book.jpg",
+  #     author: params[:author]
+  #   )
+
+  #   if params[:image]
+  #     @book.image = "#{SecureRandom.uuid}.jpg"
+  #     image = params[:image]
+  #     File.binwrite("public/book_images/#{@book.image}", image.read)
+  #   end
+
+  #   if @book.save
+  #     redirect_to root_path
+  #   else
+  #     render 'new'
+  #   end
+  # end
+
+  # def edit
+  #   @book = Book.find(params[:id])
+  # end
+
+  # def destroy
+  # end
 end
