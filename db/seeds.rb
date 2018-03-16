36744be55fe207d02872e55417ea3ae79cd9e3f9
kenishihara <stonevillagehill@gmail.com>
2018-03-16 07:47:20 +0000

    初期データ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  Book.create(
+    title: "タイトル #{i}",
+    price: i,
+    release_date: "2018-03-1#{i}",
+    image: "default_book.jpg",
+    author: "著者 #{i}"
+  )
+end
